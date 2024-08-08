57c51863413b9acb47c7bde2604c7fd4c39435cf
Hangyeol Park <hg.park.970813@gmail.com>
2024-08-08 20:39:42 +0900

    2024 08 08 google sheet 읽어오기

--- 한결.py
+++ 한결.py
@@ -1,1 +1,9 @@
-# 한결결 :
+# google sheet pandas로 읽어오는 방법
+
+import pandas as pd
+
+# https://docs.google.com/spreadsheets/d/1McH-oBzPZ8ewfyEl605wq-9b3gZHCIIBVbWHEHwNnIs/edit?gid=941998772#gid=941998772
+gsheet_url = "https://docs.google.com/spreadsheets/d/1McH-oBzPZ8ewfyEl605wq-9b3gZHCIIBVbWHEHwNnIs/gviz/tq?tqx=out:csv&sheet=ames-spot"
+
+df = pd.read_csv(gsheet_url)
+df
